c87cc2556c2111d2d14d0821e097af850f4a60e3
ravellaneda68 <ramiro.avellaneda@gmail.com>
2022-02-04 14:28:05 +0000

    05-estructura-sapui5

---
+++ holamundo/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,11 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require(["logaligroup/holamundo/test/integration/AllJourneys"
+	], function () {
+		QUnit.start();
+	});
+});
---
+++ invoice/sapui5/webapp/controller/View1.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.m.MessageToast} MessageToast
+     */
+    function (Controller, MessageToast) {
+        "use strict";
+
+        return Controller.extend("logaligroup.sapui5.controller.View1", {
+            onInit: function () {
+                
+            }
+        });
+    });
